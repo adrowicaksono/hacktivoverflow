48dbf241b86c33e761376491cb3de64583f90d4b
adrowicaksono <adrowicaksono@gmail.com>
2018-07-29 18:12:38 +0700

    90% front end

---
+++ hacktivoverflow/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <vs-row vs-w="12">
+      <HelloWorld/>
+    </vs-row>
+    <vs-row vs-w="12">
+      <Question/>
+    </vs-row>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Question from '@/components/Question.vue'
+export default {
+  name: 'home',
+  components: {
+    HelloWorld,
+    Question
+  }
+}
+</script>
+
+
