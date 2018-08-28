0bd0d169e38caec5d4cfdc58b2ae3e774e957947
adrowicaksono <adrowicaksono@gmail.com>
2018-08-28 10:11:59 +0700

    gmail-send

--- hacktivoverflow-server/middleware/gmail.js
+++ hacktivoverflow-server/middleware/gmail.js
@@ -1,12 +1,9 @@
-console.log('sendig test email')
-
-
- 
+console.log('sending test email')
 
  
 var send = require('gmail-send')({
   user: 'adrowicaks@gmail.com',
-  pass: 'xobvemyetbdgbkwp',
+  pass: '',
   to:   'adrowicaksono@gmail.com',
   // bcc: 'some-user@mail.com',            // almost any option of `nodemailer` will be passed to it
   subject: 'test subject',
